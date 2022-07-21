02fb2868472c67b523387556eb469ac946408c5b
alekseivas <a.s.vasiluev@gmail.com>
2022-07-21 15:16:26 +0000

    all file for my project

---
+++ arp-table.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+LOCKFILE=/tmp/arp-table.lock
+already_locked() {
+	echo "lock is already held, exiting"
+	exit 1
+}
+
+while(true)
+do
+exec 200>$LOCKFILE
+flock -n 200 || already_locked;
+arp -vn -i enp0s3 >> /demo/log.txt;
+date +"%H:%M:%S" >> /demo/log.txt;
+echo "---------------" >> /demo/log.txt;
+sleep 60;
+done
+
+echo "releasing lock, done"
